129ed6c317ccc94bd9c2bce55d76b252f17ae1a0
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-04-26 16:47:41 -0500

    Adding the save post funcionality

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'No Content';
 
+  onAddPost(){
+    this.newPost = this.enteredValue;
+  }
 }
