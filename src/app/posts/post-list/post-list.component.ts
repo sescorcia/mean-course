625c403b748ac71d5c16ad27b54c5ae4648e7379
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-04-27 22:51:09 -0500

    Adding post model optimization

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -11,7 +12,7 @@ export class PostListComponent implements OnInit {
   //   {title: 'Second post', content: 'This is the second post\'s content'},
   //   {title: 'Third post', content: 'This is the third post\'s content'}
   // ];
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 
   constructor() { }
 
