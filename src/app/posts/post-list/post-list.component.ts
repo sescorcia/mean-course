5758054d5fcae015e65c6ccf7f51945fbecd5397
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-05-01 10:30:14 -0500

    Adding the Post Service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -14,7 +15,7 @@ export class PostListComponent implements OnInit {
   // ];
   @Input() posts: Post[] = [];
 
-  constructor() { }
+  constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
   }
