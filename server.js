3e1b5a7d6fe14cd10c666087a47e392cf458e99c
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-05-01 22:00:41 -0500

    Creating a simple backend server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+//This sentence follows the syntax that NodeJS follows for importing
+const http = require('http');
+
+const server = http.createServer((request, respond) => {
+  respond.end('This is my first response');
+});
+
+//process.env.Port is an environment variable that is injected in runtime (set up in prod)
+server.listen(process.env.Port || 3000);
