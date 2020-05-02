0a47be7d1b8270775c692918ab67a5efd5da1d44
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-05-02 15:10:39 -0500

    Adding the express framework

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //This sentence follows the syntax that NodeJS follows for importing
 const http = require('http');
+const app = require('./backend/app');
 
-const server = http.createServer((request, respond) => {
-  respond.end('This is my first response');
-});
+
+const port = process.env.Port || 3000;
+
+app.set('port', port);
+const server = http.createServer(app);
 
 //process.env.Port is an environment variable that is injected in runtime (set up in prod)
-server.listen(process.env.Port || 3000);
+server.listen(port);
