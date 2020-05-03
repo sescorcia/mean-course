6331a04cb676b59feab404d487b56d64c8e517b7
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-05-02 23:51:59 -0500

    Wiring up the 'post' frontend method with the backend Also fixing an error in the express server configuration

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,33 @@
-const express = require('express');
+const express = require('express'); //Importing the express framework
+const bodyParser = require('body-parser'); //Importing the body-parser module
 
+//Creating the express server
 const app = express();
 
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false })); //For encoded body
+
+//Adding headers to fix the CORS error
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
   res.setHeader('Access-Control-Allow-Headers',
   'Origin, X-Requested-With, Content-Type, Accept');
-  next();
   res.setHeader('Access-Control-Allow-Methods',
   'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+  next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+//Creating the post verb to emulate an persistence action
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  })
+});
+
+//Creating the get verb to get dummy posts
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'sdaf1234',
@@ -25,7 +41,7 @@ app.use('/api/posts', (req, res, next) => {
     }
   ];
   res.status(200).json({
-    message: 'Posts fetched succesfully!',
+    message: 'Posts fetched successfully!',
     posts: posts
   });
 });
