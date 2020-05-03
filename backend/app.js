388c7e82ef173e78463a727a1978dc7ac30abfd5
Salvador Escorcia <salvador.escorcia@gmail.com>
2020-05-02 22:51:51 -0500

    Fixing the CORS error Added headers to the backend server to avoid the CORS error

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+  res.setHeader('Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+});
+
 app.use('/api/posts', (req, res, next) => {
   const posts = [
     {
